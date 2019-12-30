9d03ca47e005aeb285a180f4db06047e97742487
zhubinfeng <zhubin_feng@163.com>
2019-12-30 17:56:30 +0800

    添加游客按钮显示开关

--- SDKUISHYU/SDKUISHYU.framework/Headers/LTUIManager.h
+++ SDKUISHYU/SDKUISHYU.framework/Headers/LTUIManager.h
@@ -16,11 +16,32 @@ typedef void (^loginUIUserBlock)(LTUser *loginUser);
 
 + (instancetype)sharedInstance;
 
+/// 设置隐私和用户协议地址
+/// @param userAgreement 用户协议地址
+/// @param privacy 隐私条款地址
 -(void)linkOfUserAgreement:(NSString *)userAgreement andPrivacyLine:(NSString *)privacy;
 
+/// 弹出登录界面
+/// @param uc 展示父界面
+/// @param block 回调
 -(void)showLoginUI:(UIViewController *)uc withBlock:(loginUIUserBlock)block;
 
+/// 弹出登录框
+/// @param uc 父界面
+/// @param block 回调
+/// @param isShow 是否显示游客按钮
+-(void)showLoginUI:(UIViewController *)uc withBlock:(loginUIUserBlock)block isShowTouristButton:(BOOL)isShow;
+
+/// 切换账号弹出框
+/// @param uc 父界面
+/// @param block 回调
 -(void)userLogoutWithUI:(UIViewController *)uc withBlock:(loginUIUserBlock)block;
+
+/// 切换账号弹出框
+/// @param uc 父界面
+/// @param block 回调
+/// @param isShow 是否显示游客按钮
+-(void)userLogoutWithUI:(UIViewController *)uc withBlock:(loginUIUserBlock)block isShowTouristButton:(BOOL)isShow;
 @end
 
 NS_ASSUME_NONNULL_END
